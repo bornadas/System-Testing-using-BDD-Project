67d50587a27ee72c19480027d6be36bc872efe62
Ann-Charlotte Ekman <danielsson.maryann@gmail.com>
2021-10-12 00:18:42 +0200

    Added count-price feature and js file

--- Features/count-price.feature
+++ Features/count-price.feature
@@ -1,21 +1,11 @@
-Feature: Count total price in shpping cart
+Feature: Count total price in shopping cart
   As a user, I want the shopping cart to count correctly so that I can see the total price before I choose to shop.
-
   Background:
     Given that I am on "https://www.willys.se"
     When that I accepted the standard cookie policy
-    And I click on + button to add the first product from veckans varor
     And that we have been through the initial where to deliver popup
-    And I click on + button to add the another product from veckans varor
-    Then both the first and the last product should be in the cart
-
-
-  Scenario: The total price at the cart wagon should be increased after increasing the quantity
-    When I click on cart
-    And I click on + button to increase the quantity
-    Then the total cost should increase
-
-  Scenario: The total price at the cart wagon is decreasing when I am reducing the product quantity
-    Given that I am on "https://www.willys.se/varukorg"
-    When I click on - button to decrease the quantity
-    Then the total cost should decrease
+  Scenario: Count total price correctly in shopping cart
+    Given that I am on the Fruit category page
+    When I put a random number of each fruit that has price per piece in the cart
+    Then the mini-cart should show the correct total quantity of products
+    And the mini-cart should show correct total price
--- step-definitions/common-background.js
+++ step-definitions/common-background.js
@@ -1,15 +1,12 @@
 let slowDown = true;
-
 async function waitAWhile() {
   await driver.sleep(slowDown ? 5000 : 0);
 }
-
 module.exports = function () {
-
   this.Given(/^that I am on "([^"]*)"$/, async function (url) {
     await helpers.loadPage(url);
   });
-//for cookies policy
+  //for cookies policy
   this.When(/^that I accepted the standard cookie policy$/, async function () {
     await driver.wait(until.elementsLocated(by.css('#onetrust-accept-btn-handler')), 10000);
     let cookieAcceptButton = await driver.findElement(By.css('#onetrust-accept-btn-handler'));
@@ -19,25 +16,17 @@ module.exports = function () {
     await cookieAcceptButton.click();
     await waitAWhile();
   });
-// add first product
-  this.When(/^I click on \+ button to add the first product from veckans varor$/, async function () {
+  // remove delivery popup
+  this.When(/^that we have been through the initial where to deliver popup$/, async function () {
     let plusButton = await driver.findElement(By.css('.ax-product-quantity-plus'));
     await plusButton.click();
-       });
-// remove delivery popup
-  this.When(/^that we have been through the initial where to deliver popup$/, async function () {
+    await waitAWhile();
     await driver.wait(until.elementsLocated(by.css('.ax-delivery-widget-overlay')), 10000);
     let deliveryOverlay = await driver.findElement(By.css('.ax-delivery-widget-overlay'));
     await deliveryOverlay.click();
-    await waitAWhile();    
-  });
-// add more products
-  this.When(/^I click on \+ button to add more products from veckans varor$/, async function () { 
-     await(await(await driver.findElements(By.css('.ax-product-quantity-plus')))[4]).click(); 
-     await waitAWhile();
-     await(await(await driver.findElements(By.css('.ax-product-quantity-plus')))[2]).click(); 
-     await waitAWhile();
-     await(await(await driver.findElements(By.css('.ax-product-quantity-plus')))[6]).click(); 
-     await waitAWhile();
+    await waitAWhile();
+    let minusButton = await driver.findElement(By.css('.ax-product-quantity-minus'));
+    await minusButton.click();
+    await waitAWhile();
   });
 }
