a41cfb6463646dbc7b3cbbdd6d41cda6e805d2a2
ImMeena <meenasingh754@gmail.com>
2021-10-12 10:50:33 +0200

    updated viewing sortiment

--- step-definitions/viewing-sortiment.js
+++ step-definitions/viewing-sortiment.js
@@ -18,30 +18,39 @@ module.exports = function () {
         let findList = await $('.ax-sidemenu-heading');
         await waitAWhile();
         expect(findList).to.not.equal(null);
+        await driver.executeScript('window.scrollTo(0,1000)');
         await waitAWhile();
     });     
 
     this.Then(/^I click on Barn$/, async function () {
         await driver.findElement(By.css('a[href="/sortiment/barn"]')).click();
+        await driver.executeScript('window.scrollTo(0,1000)');
         await waitAWhile();
     });
 
     this.Then(/^I should get all the categories$/,async  function () {
         let grabCategories= await driver.wait(until.elementsLocated(by.css('[class^="AccordionNavNodestyles__StyledAccordionNavNodeList-sc-19q8af6-5 bvvGWC"]')));
         expect(grabCategories).to.not.equal(0);
+        await driver.executeScript('window.scrollTo(0,1000)');
         await waitAWhile();         
     });
 
     this.Then(/^I click on grot$/, async function () {
         let grabGrot = await driver.findElement(By.css('a[href="/sortiment/barn/grot"]'));
         await grabGrot.click();
-    });
-       
-    this.Then(/^the result should contain at least one sortiment$/, async function () {
+        await driver.executeScript('window.scrollTo(0,1000)');
         let h2Text;
         while (h2Text !== 'Gröt') {
             h2Text = await (await driver.findElement(By.css('h2'))).getText();
-            await driver.sleep(100);
+            await waitAWhile();
         };
+    });
+       
+    this.Then(/^the result should contain at least one sortiment$/, async function () {
+        await driver.wait(until.elementsLocated(by.css('.Grid_grid__1YmC6')));
+        await driver.executeScript('window.scrollTo(0,1000)');
+        let itemList = await driver.wait(until.elementsLocated(by.css('[itemtype="https://schema.org/Product"]')));
+        expect(itemList).to.not.equal(0);
+        await waitAWhile();
     }); 
 };
