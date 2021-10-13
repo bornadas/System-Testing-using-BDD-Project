b04144d298771f40d1b3c642e47fdc69e19aa24e
Sanahere <sana_baig001@outlook.com>
2021-10-13 15:46:04 +0200

    updated empty entire cart

--- Features/empty-entire-cart.feature
+++ Features/empty-entire-cart.feature
@@ -2,13 +2,14 @@ Feature: Empty entire shopping cart
   As a user, I want to be able to empty my
   entire shopping cart so that I can start over if
   I regret what I put in it completely.
+
   Background:
     Given that I am on "https://www.willys.se"
     When that I accepted the standard cookie policy
     And that we have been through the initial where to deliver popup
+
   Scenario: Empty my shopping cart
-    Given that I am on the Fruit category page
-    When I put a random number of each fruit that has price per piece in the cart
-    When I click on the shopping cart
+    When I added some product in cart
+    And I click on the shopping cart
     And I click on töm varukorg button
     Then The cart should be empty
--- step-definitions/empty-entire-cart.js
+++ step-definitions/empty-entire-cart.js
@@ -3,17 +3,25 @@ async function waitAWhile() {
     await driver.sleep(slowDown ? 5000 : 0);
 }
 module.exports = function () {
+    this.When(/^I added some product in cart$/, async function () {
+        let loadMoreButton = driver.findElement(By.css('[ax-analytics2-action="Beam_ShowMore"]'));
+        await loadMoreButton.click();
+        for (let i = 0; i < 40; i++) {
+            await (await (await driver.findElements(By.css('.ax-product-quantity-plus')))[i]).click();
+        }
+        await waitAWhile();
+    });
     this.When(/^I click on the shopping cart$/, async function () {
-        await driver.findElement(By.css('[href="https://www.willys.se/varukorg"]')).click();
+        await driver.findElement(By.css('[ax-analytics2-action="Öppna minivarukorg"]')).click();
         await waitAWhile();
     });
     this.When(/^I click on töm varukorg button$/, async function () {
-        let tomButton = await driver.findElement(By.css('[title="Töm Varukorgen"]'));
+        let tomButton = await driver.findElement(By.css('[action="vm.clearCart()"]'));
         await tomButton.click();
         await waitAWhile();
     });
     this.Then(/^The cart should be empty$/, async function () {
-        let clearButton = await driver.findElement(By.css('[title="Töm"]'));
+        let clearButton = await driver.findElement(By.css('[ng-show="dialog.ok"]'));
         await clearButton.click();
         await waitAWhile();
     });
